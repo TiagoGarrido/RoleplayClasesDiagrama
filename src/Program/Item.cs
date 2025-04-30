f15aa163225f1062bf3d2ab9ab0cf5aac3e9c506
Tiago <tiago.garrido@correo.ucu.edu.uy>
2025-04-30 19:28:48 -0300

    Durabilidad

--- src/Program/Item.cs
+++ src/Program/Item.cs
@@ -4,11 +4,27 @@ public class Item
     public string Name { get; }
     public int Attack { get; }
     public int Defense { get; }
+    public int Durability { get; private set; }
+    
 
-    public Item(string name, int attack, int defense)
+    public Item(string name, int attack, int defense, int durability)
     {
         this.Name = name;
         this.Attack = attack;
         this.Defense = defense;
+        this.Durability = durability;
+    }
+
+    public void UseItem()
+    {
+        if (Durability > 0)
+        {
+            Durability = Durability - 1;
+        }
+    }
+
+    public void RepairItem(int amount)
+    {
+        Durability = Durability + amount;
     }
 }
