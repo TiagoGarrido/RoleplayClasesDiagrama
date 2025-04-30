6bf927a9fb44efe9c7d5e9cb7c99b9f7b3d70c84
Tiago <tiago.garrido@correo.ucu.edu.uy>
2025-04-30 19:19:22 -0300

    Primer cambio Items

---
+++ src/Program/Item.cs
@@ -0,0 +1,14 @@
+﻿
+public class Item
+{
+    public string Name { get; }
+    public int Attack { get; }
+    public int Defense { get; }
+
+    public Item(string name, int attack, int defense)
+    {
+        this.Name = name;
+        this.Attack = attack;
+        this.Defense = defense;
+    }
+}
