63cc803dbb3a1ab21c82c415425ee29d74bc3e03
Santiago <st281306@fi365.ort.edu.uy>
2025-04-30 20:09:37 -0300

    Wizard more functionalities

--- src/Library/Wizard.cs
+++ src/Library/Wizard.cs
@@ -130,4 +130,84 @@ public class Wizard
         }
         return damage;
     }
+    
+    public void NewWand(Item newWand)
+    {
+        if (newWand != null)
+        {
+            wand = newWand;
+            Console.WriteLine("Equipped new wand: " + newWand.Name);
+        }
+    }
+    
+    public void RemoveWand()
+    {
+        if (wand != null)
+        {
+            Console.WriteLine("Removed wand: " + wand.Name);
+            wand = null;
+        }
+        else
+        {
+            Console.WriteLine("No wand to remove.");
+        }
+    }
+    
+    public void NewSpellbook(Item newSpellbook)
+    {
+        if (newSpellbook != null)
+        {
+            spellbook = newSpellbook;
+            Console.WriteLine("Equipped new spellbook: " + newSpellbook.Name);
+        }
+    }
+    
+    public void RemoveSpellbook()
+    {
+        if (spellbook != null)
+        {
+            Console.WriteLine("Removed spellbook: " + spellbook.Name);
+            spellbook = null;
+        }
+        else
+        {
+            Console.WriteLine("No spellbook to remove.");
+        }
+    }
+    
+    public void NewRobe(Item newRobe)
+    {
+        if (newRobe != null)
+        {
+            robe = newRobe;
+            Console.WriteLine("Equipped new robe: " + newRobe.Name);
+        }
+    }
+    
+    public void RemoveRobe()
+    {
+        if (robe != null)
+        {
+            Console.WriteLine("Removed robe: " + robe.Name);
+            robe = null;
+        }
+        else
+        {
+            Console.WriteLine("No robe to remove.");
+        }
+    }
+    
+    public void Defend()
+    {
+        if (robe != null)
+        {
+            Console.WriteLine("Defending with " + robe.Name);
+        }
+        else
+        {
+            Console.WriteLine("Cannot defend without robe.");
+        }
+    }
+    
+    
 }
