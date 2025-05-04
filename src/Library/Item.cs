b56adbd7640126c0d79336c58e858e94583c9b67
Tiago <tiago.garrido@correo.ucu.edu.uy>
2025-05-04 13:21:17 -0300

    Atributos privados main

--- src/Library/Item.cs
+++ src/Library/Item.cs
@@ -2,10 +2,10 @@
 
 public class Item
 {
-    public string Name { get; }
-    public int Attack { get; }
-    public int Defense { get; }
-    public int Durability { get; private set; }
+    private string Name { get; }
+    private int Attack { get; }
+    private int Defense { get; }
+    private int Durability { get; set; }
     
 
     public Item(string name, int attack, int defense, int durability)
@@ -27,32 +27,6 @@ public class Item
     public void RepairItem(int amount)
     {
         Durability = Durability + amount;
-    }
-
-    public class Hechizo
-    {
-        public string Name { get; }
-        public int Power { get; }
-
-        public Hechizo(string name, int power)
-        {
-            this.Name = name;
-            this.Power = power;
-        }
-    }
-
-    public class LibroHechizos
-    {
-        public List<Hechizo> hechizos = new List<Hechizo>();
-
-        public LibroHechizos(string name, int durability)
-        {
-        }
-
-        public void AgregarHechizo(Hechizo hechizo)
-        {
-            hechizos.Add(hechizo);
-        }
     }
     
 }
