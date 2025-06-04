2784d2ce76bb0bc83c3f9a95f2db263e83a3773d
Santiago <st281306@fi365.ort.edu.uy>
2025-06-04 18:27:44 -0300

    program.cs 1st refactor

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,66 +1,53 @@
 ﻿using System;
 using Library;
+using System.Collections.Generic;
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Bienvenido a la aventura de rol!");
+        // Crear personajes
+        ICharacter gandalf = new Wizard("Gandalf", 100);
+        ICharacter legolas = new Elves("Legolas", 90);
+        ICharacter gimli = new Dwarf("Gimli", 120);
+        ICharacter orco = new Enemigo("Orco", 80);
 
-        // Crear personajes y objetos
-        ICharacter enano = new Dwarf("Gimli", 100);
-        IItem martilloDeGuerra = new Martillo("Martillo de Guerra", 15,32); // Cambiar a la clase correcta
-        IItem armaduraValiriana = new Armadura("Armadura Valiriana", 20);
+        // Crear ítems
+        IItem arco = new Arco("Arco élfico", 20);
+        IItem armadura = new Armadura("Armadura pesada", 15);
+        //IMagicItem baston = new Baston("Bastón de poder", 25);
 
-        Console.WriteLine(enano.AddItem(martilloDeGuerra));
-        Console.WriteLine(enano.AddItem(armaduraValiriana));
-        ISpellbook SangreDracarica = new SpellBook("Sangre Dracárica");
-        Spell bolaDeFuego = new Spell("Bola de Fuego", 20);
-        Spell llamarada = new Spell("Llamarada", 15);
+        // Asignar ítems a personajes
+        //gandalf.AddItem(baston);
+        legolas.AddItem(arco);
+        legolas.AddItem(armadura);
+        gimli.AddItem(armadura);
 
-        Console.WriteLine(SangreDracarica.AddSpell(bolaDeFuego));
-        Console.WriteLine(SangreDracarica.AddSpell(llamarada));
+        // Mostrar info inicial
+        MostrarInfo(gandalf);
+        MostrarInfo(legolas);
+        MostrarInfo(gimli);
+        MostrarInfo(orco);
 
-        IMagicalCharacter mago = new Wizard("Gandalf", 100, SangreDracarica);
-        IMagicItem baston = new Baston("Bastón Mágico", 10);
-        IMagicItem capa = new Capa("Capa Mágica", 10);
+        // Simular combates
+        Console.WriteLine(legolas.Attack(orco));
+        Console.WriteLine(gimli.Attack(orco));
+        Console.WriteLine(gandalf.Attack(orco));
 
-        Console.WriteLine(mago.AddMagicalItem(baston));
-        Console.WriteLine(mago.AddMagicalItem(capa));
+        // Curar
+        Console.WriteLine(orco.Heal());
+        Console.WriteLine(gimli.Heal());
 
-        ISpellbook CorazonHelado = new SpellBook("Corazón Helado");
-        Spell Nevada = new Spell("Tormenta de Nieve", 20);
-        Spell picosH = new Spell("Picos Helados", 15);
+        // Mostrar info final
+        MostrarInfo(gandalf);
+        MostrarInfo(legolas);
+        MostrarInfo(gimli);
+        MostrarInfo(orco);
+    }
 
-        Console.WriteLine(CorazonHelado.AddSpell(Nevada));
-        Console.WriteLine(CorazonHelado.AddSpell(picosH));
-
-        IMagicalCharacter mago1 = new Wizard("Sauron", 100, CorazonHelado);
-        IMagicItem bastonGigante = new Baston("Bastón de Hielo", 10);
-        IMagicItem capain = new Capa("Capa de Sigilo", 0);
-
-        Console.WriteLine(mago1.AddMagicalItem(bastonGigante));
-        Console.WriteLine(mago1.AddMagicalItem(capain));
-
-        ICharacter elfo= new Elves("Legolas", 100);
-        IItem arco = new Arco("Arco de yggdrasil", 12);
-        IItem tunicaElfica = new Armadura("Túnica Élfica", 8);
-
-        Console.WriteLine(elfo.AddItem(arco));
-        Console.WriteLine(elfo.AddItem(tunicaElfica));
-
-        // Simulación de combate
-        Console.WriteLine( mago.Attack(enano));
-        Console.WriteLine(mago.CastSpell(enano, bolaDeFuego));
-        Console.WriteLine(mago1.CastSpell(mago, Nevada));
-        Console.WriteLine(enano.Attack(elfo));
-        Console.WriteLine(elfo.Attack(mago1));
-        Console.WriteLine(enano.Heal());
-
-        // Mostrar información final
-        Console.WriteLine(enano.GetInfo());
-        Console.WriteLine(mago.GetInfo());
-        Console.WriteLine(mago1.GetInfo());
-        Console.WriteLine(elfo.GetInfo());
+    static void MostrarInfo(ICharacter character)
+    {
+        Console.WriteLine(character.GetInfo());
+        Console.WriteLine(new string('-', 40));
     }
 }
