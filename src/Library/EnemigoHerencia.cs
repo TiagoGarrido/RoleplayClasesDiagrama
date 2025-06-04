35e8603279a983c0a544be9dc69e183833702f46
thiagocastillo <Thiago.castillo.cbt@gmail.com>
2025-06-04 12:55:18 -0300

    Heroeherencia,enemigoherencia

---
+++ src/Library/EnemigoHerencia.cs
@@ -0,0 +1,6 @@
+namespace Library;
+
+public abstract class EnemigoHerencia : Personaje
+{
+    public EnemigoHerencia(string name, int initialHealth) : base(name, initialHealth) { }
+}
---
+++ src/Library/HeroeHerencia.cs
@@ -0,0 +1,7 @@
+
+namespace Library;
+
+public abstract class HeroeHerencia : Personaje
+{
+    public HeroeHerencia(string name, int initialHealth) : base(name, initialHealth) { }
+}
